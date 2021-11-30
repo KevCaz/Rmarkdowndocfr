c0cf3372714adbb7156447b9007c6d897b1f56b3
Kevin Cazelles <kevcaz@protonmail.com>
2021-11-30 09:22:03 -0500

    :pencil: use source() to load external file

---
+++ scripts/scr_externe1.R
@@ -0,0 +1,4 @@
+var1 <- 2
+var2 <- 10*var1
+var3 <- cos(var2)
+var4 <- sin(var2)
---
+++ scripts/zzz.R
@@ -0,0 +1,7 @@
+url_this <- function(adr, base = "https://kevcaz.github.io/Rmarkdowndocfr/") {
+  paste0("<", base, adr, ">")
+}
+
+url_this_gh <- function(adr, base = "https://github.com/KevCaz/Rmarkdowndocfr") {
+  paste0("<", base, adr, ">")
+}
