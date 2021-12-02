14f1d8aaf1e3d5168bf6542189c3692f69dcbe9c
Kevin Cazelles <kevcaz@protonmail.com>
2021-12-02 09:27:32 -0500

    :pencil: load external script

---
+++ scripts/scr_externe2.R
@@ -0,0 +1,9 @@
+# @knitr part1
+var1 <- 2
+var2 <- 10*var1
+# @knitr end_part1
+
+# @knitr part2
+var3 <- cos(var2)
+var4 <- sin(var2)
+# @knitr end_part2
--- scripts/zzz.R
+++ scripts/zzz.R
@@ -2,6 +2,8 @@ url_this <- function(adr, base = "https://kevcaz.github.io/Rmarkdowndocfr/") {
   paste0("<", base, adr, ">")
 }
 
-url_this_gh <- function(adr, base = "https://github.com/KevCaz/Rmarkdowndocfr") {
-  paste0("<", base, adr, ">")
+url_this_gh <- function(adr, lkn = NULL, base = "https://github.com/KevCaz/Rmarkdowndocfr") {
+  if (!is.null(lkn)) {
+    paste0("[", lkn, "](", base, adr, ")")
+  } else paste0("<", base, adr, ">")
 }
