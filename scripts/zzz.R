8cb1d5449d17257d7eb59e76b1b311d39253fc15
Kevin Cazelles <kevcaz@protonmail.com>
2021-12-17 10:15:34 -0500

    :pencil: add info for external lang

--- scripts/zzz.R
+++ scripts/zzz.R
@@ -2,7 +2,7 @@ url_this <- function(adr, base = "https://kevcaz.github.io/Rmarkdowndocfr/") {
   paste0("<", base, adr, ">")
 }
 
-url_this_gh <- function(adr, lkn = NULL, base = "https://github.com/KevCaz/Rmarkdowndocfr") {
+url_this_gh <- function(adr, lkn = NULL, base = "https://github.com/KevCaz/Rmarkdowndocfr/") {
   if (!is.null(lkn)) {
     paste0("[", lkn, "](", base, adr, ")")
   } else paste0("<", base, adr, ">")
